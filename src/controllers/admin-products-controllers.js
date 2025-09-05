7d0be88a636fbbf7b884533d836dc1951a12f0e8
Muhamad Nur Rizki <farianhab123@gmail.com>
2025-09-05 10:15:59 +0700

    menambahkan fitur update product by id

--- src/controllers/admin-products-controllers.js
+++ src/controllers/admin-products-controllers.js
@@ -69,3 +69,58 @@ export const getProductByIdController = async (req, res) => {
     console.log("Terjadi kesalahan:", error);
   }
 };
+
+export const updateProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description, price, stock, type } = req.body;
+    const { image } = req.files;
+
+    if (!id) {
+      return res.status(400).json({ message: "Id tidak ditemukan" });
+    }
+
+    if (!name || !description || !price || !stock || !image || !type) {
+      return res.status(400).json({ message: "Semua field wajib diisi" });
+    }
+
+    const imageProduct = await product.findById(id, {
+      imageUrl: 1,
+      imageId: 1,
+    });
+
+    if (!imageProduct) {
+      return res.status(404).json({ message: "Image not found" });
+    }
+
+    let updateData = { name, description, price, stock, type };
+
+    if (image) {
+      // hapus gambar lama
+      if (imageProduct.imageId) {
+        await cloudinary.uploader.destroy(imageProduct.imageId);
+      }
+
+      // upload gambar baru
+      const uploadImage = await cloudinary.uploader.upload(image.tempFilePath, {
+        folder: "products",
+        width: 300,
+        crop: "scale",
+      });
+
+      updateData.imageUrl = uploadImage.secure_url;
+      updateData.imageId = uploadImage.public_id;
+    }
+
+    // update db
+    const updateProduct = await product.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+
+    return res
+      .status(201)
+      .json({ message: "Product berhasil diupdate", updateProduct });
+  } catch (error) {
+    console.log("Terjadi kesalahan:", error);
+  }
+};
--- src/routes/admin-products.js
+++ src/routes/admin-products.js
@@ -3,11 +3,13 @@ import {
   createProductController,
   getProductByIdController,
   getProductsController,
+  updateProductById,
 } from "../controllers/admin-products-controllers.js";
 const admin = express.Router();
 
 admin.post("/api/products/add", createProductController);
 admin.get("/api/products", getProductsController);
 admin.get("/api/products/:id", getProductByIdController);
+admin.put("/api/products/edit/:id", updateProductById);
 
 export default admin;
